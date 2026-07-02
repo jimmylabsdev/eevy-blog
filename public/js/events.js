2b4ca08f047cb5917759b9dd08aef7a48b4259cd
Praveen <jimmy.labs.dev@gmail.com>
2026-07-02 19:37:22 +0530

    v3-full-commit

---
+++ public/js/events.js
@@ -0,0 +1,122 @@
+// ============================
+// LANDING
+// ============================
+
+function assmt_landing_view() {
+    gtag('event', 'landing_view');
+}
+
+function assmt_landing_cta_click(language) {
+    gtag('event', 'landing_cta_click', {
+        language: language
+    });
+}
+
+
+// ============================
+// ASSESSMENT
+// ============================
+
+function assmt_start() {
+    gtag('event', 'assessment_start');
+}
+
+
+// ============================
+// TOP 5 EVs
+// ============================
+
+function assmt_top5_view() {
+    gtag('event', 'top5_view');
+}
+
+function assmt_top5_continue() {
+    gtag('event', 'top5_continue');
+}
+
+
+// ============================
+// TOP 3 EVs
+// ============================
+
+function assmt_top3_view() {
+    gtag('event', 'top3_view');
+}
+
+function assmt_top3_continue() {
+    gtag('event', 'top3_continue');
+}
+
+
+// ============================
+// FINANCE
+// ============================
+
+function assmt_finance_view() {
+    gtag('event', 'finance_view');
+}
+
+function assmt_finance_continue() {
+    gtag('event', 'finance_continue');
+}
+
+
+// ============================
+// INSURANCE
+// ============================
+
+function assmt_insurance_view() {
+    gtag('event', 'insurance_view');
+}
+
+function assmt_insurance_continue() {
+    gtag('event', 'insurance_continue');
+}
+
+
+// ============================
+// CHARGING READINESS
+// ============================
+
+function assmt_charging_view() {
+    gtag('event', 'charging_view');
+}
+
+function assmt_charging_continue() {
+    gtag('event', 'charging_continue');
+}
+
+
+// ============================
+// SHOWROOM CHECKLIST
+// ============================
+
+function assmt_showroom_view() {
+    gtag('event', 'showroom_view');
+}
+
+function assmt_showroom_continue() {
+    gtag('event', 'showroom_continue');
+}
+
+
+// ============================
+// LEAD PAGE
+// ============================
+
+function assmt_lead_view() {
+    gtag('event', 'lead_view');
+}
+
+function assmt_lead_submitted() {
+    gtag('event', 'lead_submitted');
+}
+
+
+// ============================
+// FINAL BUYING KIT
+// ============================
+
+function assmt_buying_kit_view() {
+    gtag('event', 'buying_kit_view');
+}
